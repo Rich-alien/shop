bef51180a6cd26e04bca19c02dcfbb4279356492
s.khakimov <s.khakimov@iva.ru>
2026-07-16 23:24:25 +0300

    update version and add @ngrx/signals

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,10 +1,10 @@
-import {Component, signal} from '@angular/core';
-import {RouterOutlet} from '@angular/router';
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  template: '<router-outlet />',
+  templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
